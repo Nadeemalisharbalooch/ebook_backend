bb31624b30acf377ac68afbf3a0b167c893c44b0
khaandev <mkhan.thecoder@gmail.com>
2025-06-19 13:20:49 +0500

    fix file name

--- app/Http/Requests/User/updatePassword.php
+++ app/Http/Requests/User/updatePassword.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\User;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class updatePassword extends FormRequest
+class UpdatePassword extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
