6479d568db4e629562bbc6f54ad2e4ddd6a1a5f0
nadeemalisharbalooch <nadeemalisharbalooch@gmail.com>
2025-08-07 10:52:32 -0700

    change user migration aa

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->boolean('is_accept_terms')->default(false);
             $table->boolean('is_admin');
             $table->boolean('is_active')->default(false);
-            $table->boolean('is_suspended')->default(true);
+            $table->boolean('is_suspended')->default(false);
             $table->boolean('is_impersonating')->default(false);
 
             $table->rememberToken();
