c3ab9e927820d39261463404d8cb1e2d86f61e27
nadeemalisharbalooch <nadeemalisharbalooch@gmail.com>
2025-08-10 16:07:20 +0500

    add countries relations change migration

--- database/seeders/WorldDataSeeder.php
+++ database/seeders/WorldDataSeeder.php
@@ -17,13 +17,17 @@ class WorldDataSeeder extends Seeder
             'cities.sql',
         ];
 
-        // If you only have 3 files, remove the top two from $files
-
         DB::statement('SET FOREIGN_KEY_CHECKS=0;'); // disable FK checks during import
         foreach ($files as $file) {
             $path = database_path('seeders/sql/' . $file);
             if (File::exists($path)) {
                 $sql = File::get($path);
+
+                // MySQL dump ke special commands remove karo
+                $sql = preg_replace('/\/\*![0-9]+.*?\*\//s', '', $sql); // /*! ... */ comments hataye
+                $sql = preg_replace('/-- Dump completed on.*/', '', $sql); // dump completed line hataye
+                $sql = str_replace(');)', ');', $sql); // extra parenthesis fix
+
                 DB::unprepared($sql);
                 $this->command->info("Imported: {$file}");
             } else {
