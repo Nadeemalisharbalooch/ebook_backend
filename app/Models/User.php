2d7d0e9a47c1fd9839bf1cb58a1079eca6a817c6
nadeemalisharbalooch <nadeemalisharbalooch@gmail.com>
2025-08-12 21:57:24 +0500

    add book crude with soft delete

--- app/Models/User.php
+++ app/Models/User.php
@@ -105,7 +105,7 @@ class User extends Authenticatable
 
             event(new CodeVerificationEvent($this));
 
-            return redirect(env('FRONTEND_URL'));
+            return redirect()->away(env('FRONTEND_URL'));
         }
 
         if (! $this->is_active) {
