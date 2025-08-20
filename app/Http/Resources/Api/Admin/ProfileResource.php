5550d50f9893af670dcf3e8219d9e508af745ffa
nadeemalisharbalooch <nadeemalisharbalooch@gmail.com>
2025-08-20 22:38:45 +0500

    add profile resources

--- app/Http/Resources/Api/Admin/ProfileResource.php
+++ app/Http/Resources/Api/Admin/ProfileResource.php
@@ -12,21 +12,29 @@ class ProfileResource extends JsonResource
      *
      * @return array<string, mixed>
      */
-   public function toArray($request)
+
+       public function toArray($request)
     {
         return [
-            'id'        => $this->id,
-            'user_id'   => $this->user_id,
-            'avatar'    => $this->avatar,
-            'gender'    => $this->gender,
-            'dob'       => $this->dob,
-            'phone'     => $this->phone,
-            'zipcode'   => $this->zipcode,
-            'street'    => $this->street,
-            'created_at'=> $this->created_at,
-            'updated_at'=> $this->updated_at,
+            'id'               => $this->id,
+            'username'         => $this->username,
+            'first_name'       => $this->first_name,
+            'last_name'        => $this->last_name,
+            'email'            => $this->email,
+            'email_verified_at'=> $this->email_verified_at,
+            'verification_code'=> $this->verification_code,
+            'role'             => $this->role,
+            'is_accept_terms'  => $this->is_accept_terms,
+            'is_admin'         => $this->is_admin,
+            'is_active'        => $this->is_active,
+            'is_suspended'     => $this->is_suspended,
+            'is_impersonating' => $this->is_impersonating,
+            'deleted_at'       => $this->deleted_at,
+            'created_at'       => $this->created_at,
+            'updated_at'       => $this->updated_at,
 
-            // Relations
+            // nested profile
+            'profile' => new ProfileResource($this->whenLoaded('profile')),
             'country'   => $this->whenLoaded('country'),
             'state'     => $this->whenLoaded('state'),
             'city'      => $this->whenLoaded('city'),
